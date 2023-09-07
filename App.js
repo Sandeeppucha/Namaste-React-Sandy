1098b1750d714897ad8fdd0899c958821e3fde93
Sandeep Pucha <sandeepsai1509@gmail.com>
2023-09-07 17:27:54 +0530

    added npm and parcel to project

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const parent = React.createElement('div', {}, 
+    React.createElement('h1', {}, 'Hello World Welcome to Namaste React'
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(parent)
